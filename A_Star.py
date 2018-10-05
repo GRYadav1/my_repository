e769413bab96ea1dea71cc55f2e9f930d3be65eb
Saumy Dadhich <sdadhich@uncc.edu>
2018-10-05 19:32:23 -0400

    comment addition  and redundant code removal

--- A_Star.py
+++ A_Star.py
@@ -11,23 +11,16 @@ import copy
 import itertools
 from copy import deepcopy
 
-goal = [[1,2,3],[4,5,6],[7,8,0]] 
 
-def printArr(list1):
-    
-    for i in list1:
-        print(i.inputA, "=>",i.f)
-        print("\t\t|")
-    print(goal)   
-    
+#method to print the path to reach a given node
 def printPath(childObj):
-     
      while(childObj != None):  
           print(childObj.inputA,"=>",childObj.f)
           childObj= childObj.Parent
           print("\t\t|") 
      print("\t\tInitialStage")    
-     
+
+#class to define a node and its characteristics (state, heuristic, actual cost, parent)     
 class PuzzleNode:
     
     def _init_(self):
@@ -43,13 +36,12 @@ class PuzzleNode:
         print(arr)  
          
     def eqate(self,arr):
-        self.inputA = arr          
-        
+        self.inputA = arr                  
 
-
+#Method to take initial state, goal state and heuristic type as input
 def takeInput():
-    
     inputA=[]
+    goalState = []
     inputString = input("Enter Array :");
     k=0
     for i in range(0,3):
@@ -58,10 +50,23 @@ def takeInput():
             row.append(int(inputString[k]))
             k=k+1
         inputA.append(row)
-        
-    return inputA
-def find(inputA, key):
 
+    inputString = []
+    inputString = input("Enter Goal State :")
+    k=0
+    for i in range(0,3):
+        row =[]
+        for j in range(0,3):
+            row.append(int(inputString[k]))
+            k=k+1
+        goalState.append(row)
+
+    heuristic_type = input("Enter 1 for Manhattan Heuristic or 2 for Misplaced Tile Heuristic")
+        
+    return inputA, goalState, heuristic_type
+
+#method to return the coordinates/indices of a given number
+def find(inputA, key):
         if key < 0 or key > 8:
             raise Exception("Out of Range")
 
@@ -70,14 +75,13 @@ def find(inputA, key):
                 if inputA[r][c] == key:
                     return r, c
 
+#method to generate all possible moves from the current state
 def getPossibleMovies(inpArray):
-    
     row, col = find(inpArray,0)
     moves = []
     if(row > 0 ):            
           r = row - 1
           moves.append((r,col))
-        
     if(col > 0):
           c = col - 1
           moves.append((row,c))  
@@ -89,15 +93,9 @@ def getPossibleMovies(inpArray):
           moves.append((row,c))
                        
     return moves
-                
-def Cloning(li1): 
-    A = []
-    for i in li1:
-         A.append(i)
-    return  A
-                
-def manhatten_heu(initalStage, GoalStage):   
-    
+ 
+#method to calculate the manhattan distance heuristic                
+def manhatten_heu(initalStage, GoalStage):
     indexInitial = []
     indexGoal = []
     for i in range(9):
@@ -114,13 +112,18 @@ def manhatten_heu(initalStage, GoalStage):
                
     return h;
 
+#method to calculate the misplaced tile heuristic
+def misplacedTile_heu(initialStage, GoalStage):
+	h = 0
+	for x in range(len(initialStage)):
+		for y in range(len(initialStage[x])):
+			if initialStage[x][y] != GoalStage[x][y]:
+				h+=1
+	return h
           
+#method to generate a child node
 def makeMove(input1,target): 
-
     inputA = copy.deepcopy(input1)
-    '''inputA=[]
-    for i in input1:
-          inputA.append(i)'''
     r0,c0=find(inputA,0)
     rt,ct = target
     
@@ -128,8 +131,8 @@ def makeMove(input1,target):
     print(inputA)
     return inputA
 
-def _is_visited(node,fringe,visited_or_current):
-    
+#method to check if the node has already been explored or is in the fringe; returns -1 if the node is not a duplicate
+def _is_visited(node,fringe):
     tempVis = copy.deepcopy(fringe)
     i = 0
     while(len( tempVis)>0):
@@ -140,19 +143,20 @@ def _is_visited(node,fringe,visited_or_current):
          
     return -1   
 
+#method to check if the duplicate node has less cost, if yes then add it again to the fringe
 def check_f_and_swap(fringe,node,index):
-              
         if(fringe[index].f > node.f):
             fringe[index] = node
             return 0
         return -1
-    
-def A_Star(initialStage):
+
+#method which starts the A* Algorithm    
+def A_Star(initialStage, goal, heuristic_type):
     count = 0
     visited = []
     current = []
-
-    initialStage.h = manhatten_heu(initialStage.inputA, goal)  
+    #heuristic calculation for the initial state
+    initialStage.h = manhatten_heu(initialStage.inputA, goal) if heuristic_type == 1 else misplacedTile_heu(initialStage.inputA, goal)
     initialStage.g = count
     initialStage.f = initialStage.g + initialStage.h
     initialStage.Parent = None   
@@ -161,51 +165,52 @@ def A_Star(initialStage):
     moves = []
     #i=0
     
-    
     while(len(current) >0 ):
           print(count)
           currentElement = current.pop(0)
-          visited.append(currentElement)
+          visited.append(currentElement)	#adding the current node to visited/explored nodes
           count=count+1
-          moves =getPossibleMovies(currentElement.inputA)
+          moves =getPossibleMovies(currentElement.inputA)	#method call to generate all the possible moves from the current state
 
+          #loop to generate all the children nodes from the current node
           for i in moves:
             childObj = PuzzleNode()  
-            childObj.g = currentElement.g+1
-            childObj.inputA=makeMove( currentElement.inputA,i)
-            childObj.h = manhatten_heu(childObj.inputA,goal)
-            childObj.f = childObj.g + childObj.h
+            childObj.g = currentElement.g+1	#adding the actual cost to the child
+            childObj.inputA=makeMove( currentElement.inputA,i)	#generate the child from the current node
+            childObj.h = manhatten_heu(childObj.inputA,goal) if heuristic_type == 1 else misplacedTile_heu(childObj.inputA, goal)
+            childObj.f = childObj.g + childObj.h #calculation of the heuristic (actual cost + chosen heuristic)
             childObj.Parent = currentElement
             
+            #condition to check if the goal state is reached
             if (childObj.inputA == goal):
                       print ("Bingo...................!!!!!!!")
                       print ("\nExpanded Nodes/Solution Path--\n")
-                      #printArr(visited)
-                      printPath(childObj)
+                      printPath(childObj) #print the entire path to the goal state
                       del(current)
                       return
-                  
-            if_visited_index = _is_visited(childObj,visited,1)   
+            
+            #checking if the child is already present in the visited nodes list      
+            if_visited_index = _is_visited(childObj,visited)   
             if( if_visited_index != -1):
                     check_f_and_swap(visited,childObj,if_visited_index)
             else:
                 current.append(childObj)
-                #print(childObj.inputA)
             
-            if_current_index = _is_visited(childObj,current, 0)
+            #checking if the child is already present in the fringe
+            if_current_index = _is_visited(childObj,current)
             if( if_current_index != -1):
                 check_f_and_swap(current, childObj, if_current_index)
                      
+            #sort the fringe in the increasing order of their calculated heuristics
             current = sorted(current, key=lambda p: p.f)
              
                  
-   
+#main method   
 def main():
-
-   inputA = takeInput()
-   p = PuzzleNode();
+   inputA, goal, heuristic_type = takeInput()	#method call to take all the required inputs
+   p = PuzzleNode();	#method call to create the initial node
    p.eqate(inputA)
    p.printA(p.inputA) 
-   A_Star(p)
-   #print(manhatten_heu(p.inputA, goal))
+   A_Star(p, goal, heuristic_type)	
+
 main()    
